3094d5612b7f62c74cb0380417e3810f6f7ab33a
Shakshi3104 <pikanchisato@yahoo.co.jp>
2021-02-28 13:49:17 +0900

    Initial Commit

---
+++ ANATAforDREAM/ANATAforDREAMApp.swift
@@ -0,0 +1,17 @@
+//
+//  ANATAforDREAMApp.swift
+//  ANATAforDREAM
+//
+//  Created by MacBook Pro on 2021/02/28.
+//
+
+import SwiftUI
+
+@main
+struct ANATAforDREAMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
